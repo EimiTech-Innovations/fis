0f017add66f5d867264d2abfc9f0e42bc6565a66
MTouthang <huebart16@gmail.com>
2024-11-05 00:11:11 +0530

    fix: team section

--- client/src/Components/Team.tsx
+++ client/src/Components/Team.tsx
@@ -1,12 +1,14 @@
 import React from "react";
+import profileIcon from "../assets/profile.png"
+import profileIcon2 from "../assets/profile2.png"
+import profileIcon3 from "../assets/profile3.png"
 
 const Team: React.FC = () => {
   return (
-    <div className="flex w-4/5 h-screen gap-10 mx-auto place-content-center">
-      <div className="w-1/2 place-content-center">
+    <div className="flex flex-col w-4/5 gap-10 py-10 mx-auto md:flex-row place-content-center">
+      <div className="md:w-1/2 place-content-center">
         <h3 className="text-2xl font-bold"> Meet the Team</h3>
         <p>
-          {" "}
           Meet the vibrant team behind the organisation vision, where each
           member contributes their expertise and enthusiasm to every project and
           initiative. With diverse backgrounds and skills, we unite to innovate
@@ -15,82 +17,40 @@ const Team: React.FC = () => {
         </p>
       </div>
 
-      <section className="content-center text-gray-600 body-font">
+      <section className="content-center text-gray-600 body-font">-*
         <div className="flex flex-wrap justify-center py-auto ">
-          <div className="p-4 lg:w-1/4 md:w-1/2 ">
-            <div className="relative flex flex-col items-center h-56 text-center group">
+          <div className="p-2 md:p-4 lg:w-1/3 md:w-1/2">
+            <div className="relative flex flex-col items-center text-center w-36 md:w-full md:h-56 group">
               <img
-                alt="team"
+                alt="Max Touthang"
                 className="flex-shrink-0 object-cover object-center w-full h-56 rounded-lg group-hover:opacity-30 "
-                src="https://dummyimage.com/202x202"
+                src={profileIcon}
               />
-              <h1 className="absolute hidden transition ease-in-out delay-150 top-36 group-hover:block">
-                {" "}
-                Mamang Touthang
+              <h1 className="absolute transition ease-in-out delay-150 shadow-sm md:hidden top-36 group-hover:block">
+                Max Touthang
               </h1>
             </div>
           </div>
-          <div className="p-4 lg:w-1/4 md:w-1/2 ">
-            <div className="relative flex flex-col items-center h-56 text-center group">
+          <div className="p-2 md:p-4 lg:w-1/3 md:w-1/2 ">
+            <div className="relative flex flex-col items-center text-center w-36 md:w-full md:h-56 group">
               <img
-                alt="team"
+                alt="Marcus Chongloi"
                 className="flex-shrink-0 object-cover object-center w-full h-56 rounded-lg group-hover:opacity-30 "
-                src="https://dummyimage.com/202x202"
+                src={profileIcon2}
               />
-              <h1 className="absolute hidden transition ease-in-out delay-150 top-36 group-hover:block">
-                {" "}
-                Mamang Touthang
+              <h1 className="absolute transition ease-in-out delay-150 shadow-sm md:hidden top-36 group-hover:block">
+                Marcus Chongloi
               </h1>
             </div>
           </div>
-          <div className="p-4 lg:w-1/4 md:w-1/2 ">
-            <div className="relative flex flex-col items-center h-56 text-center group">
+          <div className="p-2 md:p-4 lg:w-1/3 md:w-1/2 ">
+            <div className="relative flex flex-col items-center text-center w-36 md:h-56 group md:w-full">
               <img
-                alt="team"
+                alt="Mamang Touthang"
                 className="flex-shrink-0 object-cover object-center w-full h-56 rounded-lg group-hover:opacity-30 "
-                src="https://dummyimage.com/202x202"
+                src={profileIcon3}
               />
-              <h1 className="absolute hidden transition ease-in-out delay-150 top-36 group-hover:block">
-                {" "}
-                Mamang Touthang
-              </h1>
-            </div>
-          </div>
-          <div className="p-4 lg:w-1/4 md:w-1/2 ">
-            <div className="relative flex flex-col items-center h-56 text-center group">
-              <img
-                alt="team"
-                className="flex-shrink-0 object-cover object-center w-full h-56 rounded-lg group-hover:opacity-30 "
-                src="https://dummyimage.com/202x202"
-              />
-              <h1 className="absolute hidden transition ease-in-out delay-150 top-36 group-hover:block">
-                {" "}
-                Mamang Touthang
-              </h1>
-            </div>
-          </div>
-          <div className="p-4 lg:w-1/4 md:w-1/2 ">
-            <div className="relative flex flex-col items-center h-56 text-center group">
-              <img
-                alt="team"
-                className="flex-shrink-0 object-cover object-center w-full h-56 rounded-lg group-hover:opacity-30 "
-                src="https://dummyimage.com/202x202"
-              />
-              <h1 className="absolute hidden transition ease-in-out delay-150 top-36 group-hover:block">
-                {" "}
-                Mamang Touthang
-              </h1>
-            </div>
-          </div>
-          <div className="p-4 lg:w-1/4 md:w-1/2 ">
-            <div className="relative flex flex-col items-center h-56 text-center group">
-              <img
-                alt="team"
-                className="flex-shrink-0 object-cover object-center w-full h-56 rounded-lg group-hover:opacity-30 "
-                src="https://dummyimage.com/202x202"
-              />
-              <h1 className="absolute hidden transition ease-in-out delay-150 top-36 group-hover:block">
-                {" "}
+              <h1 className="absolute transition ease-in-out delay-150 shadow-sm md:hidden top-36 group-hover:block">
                 Mamang Touthang
               </h1>
             </div>
