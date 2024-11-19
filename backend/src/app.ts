2271a168af0c4628fcb5d7b8b0bf4763a1d5b754
MTouthang <huebart16@gmail.com>
2024-11-20 01:14:55 +0530

    feat: route setup

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import express, { Application } from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import dbConnect from './config/db';
+import { configValues } from './config';
 
 //connect to the db
 dbConnect();
@@ -21,8 +22,44 @@ const createApp = (): Application => {
   // Use the custom format
   app.use(morgan('myCustomFormat'));
 
-  app.get('/', (req, res) => {
-    res.send('Sever is healthy');
+  /**
+   * @SERVER_STATUS
+   * @ROUTE @GET {{URL}}/api/v1/ping
+   * @DESC Returns response 200 with message pong if api is working
+   * @ACCESS Public
+   */
+  app.get(`/api/${configValues.PREFIX}/ping`, (_req, res) => {
+    res.send({
+      success: true,
+      message: 'Pong',
+    });
+  });
+
+  /**
+   * @SERVER_MODE
+   * @ROUTE @GET {{URL}}/api/v1/ping
+   * @DESC Returns response 200 with application mode
+   * @ACCESS Public
+   */
+  app.get(`/api/${configValues.PREFIX}/mode`, (_req, res) => {
+    res.status(200).json({
+      success: true,
+      environment:
+        process.env.NODE_ENV === 'prod' ? 'production' : 'development',
+    });
+  });
+
+  /**
+   * @SERVER_ROUTE_NOT_DEFINE
+   * @ROUTE @GET {{URL}}/*
+   * @DESC Returns response 404 with route not available
+   * @ACCESS Public
+   */
+  app.all('*', (_req, res) => {
+    res.status(404).json({
+      success: true,
+      message: 'Route not found',
+    });
   });
 
   return app;
--- backend/src/helper/apiError.helper.ts
+++ backend/src/helper/apiError.helper.ts
@@ -1,10 +1,21 @@
-class ApiError extends Error {
+export interface IApiError extends Error {
   statusCode: number;
-
-  constructor(statusCode: number, message: string) {
-    super(message);
-    this.statusCode = statusCode;
-  }
 }
 
-export default ApiError;
+export class ApiError extends Error implements IApiError {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  keyvalue(keyvalue: unknown) {
+    throw new Error('Method not implemented');
+  }
+
+  code: number | undefined;
+  path: unknown;
+  constructor(
+    message: string,
+    public statusCode: number
+  ) {
+    super(message);
+    this.code = statusCode;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
