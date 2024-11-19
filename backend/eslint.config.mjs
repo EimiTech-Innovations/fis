a2a2d1dc06897c60c415d06cf0e83277e683079c
MTouthang <huebart16@gmail.com>
2024-11-19 22:40:45 +0530

    refactor: eslint

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,12 +1,21 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  {
+    languageOptions: { globals: globals.browser },
+
+    ignores: [
+      './dist',
+      './node_modules',
+      '.env',
+      '.env.example',
+      './gitignore',
+    ],
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
