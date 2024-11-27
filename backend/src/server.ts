817be9f5ec14c3fab62237b4990955ace0115ab8
MTouthang <huebart16@gmail.com>
2024-11-27 21:13:41 +0530

    refactor: app instance

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,11 @@
-import createApp from './app';
+import app from './app';
 import { configValues } from './config';
-
-export const app1 = createApp();
 
 async function bootstrap() {
   try {
     // await mongoose.connect(config.db_uri as string)
 
-    app1.listen(configValues.PORT, () => {
+    app.listen(configValues.PORT, () => {
       console.log(
         `Server is running on ${configValues.NODE_ENV}://localhost:${configValues.PORT}`
       );
