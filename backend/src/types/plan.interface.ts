4dddb53b9673b416b138b2cf9e356575186e3f6c
MTouthang <huebart16@gmail.com>
2025-08-04 20:06:47 +0530

    feat: plan and client services

---
+++ backend/src/types/plan.interface.ts
@@ -0,0 +1,17 @@
+import Mongoose from 'mongoose';
+
+export enum BillingCycle {
+  MONTHLY = 'MONTHLY',
+  YEARLY = 'YEARLY',
+  ONE_TIME = 'ONE_TIME',
+}
+
+export interface IPlan extends Mongoose.Document {
+  name: string;
+  description?: string;
+  price: number;
+  currency: string;
+  isActive: boolean;
+  billingCycle: BillingCycle;
+  isFree: boolean;
+}
